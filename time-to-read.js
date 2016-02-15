d24a21631ad7113253df05afa0952edbfde1b300
bhare1987 <brandonlhare@gmail.com>
2016-02-15 15:11:10 -0500

    added flag

--- time-to-read.js
+++ time-to-read.js
@@ -1,4 +1,5 @@
-function timeToRead(wpm){
+var readTime = {
+timeToRead: function(wpm){
   var args = Array.prototype.slice.call(arguments),
       content = document.querySelectorAll("article p"),
       contentArray = Array.prototype.slice.call(content),
@@ -18,20 +19,36 @@ function timeToRead(wpm){
       });
       //Regex from http://stackoverflow.com/questions/4328500/how-can-i-strip-all-punctuation-from-a-string-in-javascript-using-regex
       totalContentArray = totalContent.trim().replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").split(" ");
-      console.log(totalContentArray);
       readMinutes = Math.floor(totalContentArray.length / wps * 60);
       readSeconds = Math.floor(totalContentArray.length % wps / 60);
       readTime.push(readMinutes, readSeconds);
       return readTime;
-}
-
-function timeToReadView() {
+},
+timeToReadView: function(){
   var body = document.getElementsByTagName('body')[0],
-      time = timeToRead(),
-      container = document.createElement('div');
-      container.className = "time-to-read-view";
-      container.textContent = "Time to read: " + time[0] + " minutes, " + time[1] + " seconds";
-      body.appendChild(container);
+      time = readTime.timeToRead(),
+      container = document.createElement('div'),
+      flag = readTime.timeToReadFlag;
+    container.className = "time-to-read-view";
+    container.textContent = "Time to read: " + time[0] + " minutes, " + time[1] + " seconds";
+    body.appendChild(container);
+},
+timeToReadFlag: true,
+setFlag: function(flag) {
+  var newContainer = document.querySelectorAll('.time-to-read-view')[0];
+  console.log(newContainer);
+  readTime.timeToReadFlag = flag;
+  if (readTime.timeToReadFlag === false) {
+    newContainer.className += " hide";
+  } else if (flag === true) {
+    // class manipulation from http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
+    if (newContainer.classList.contains("hide")) {
+      newContainer.classList.remove("hide");
+    }
+  }
+}
 }
 
-timeToReadView();
+
+
+readTime.timeToReadView();
